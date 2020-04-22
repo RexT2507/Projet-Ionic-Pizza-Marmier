18e31f6b70f1a9e8cc8e8bcccb6aab65dffc2474
RexT2507 <baptiste.marmier@ynov.com>
2020-04-22 20:23:31 +0200

    [DEV] Ajout d'un ingrédient fonctionnel

--- src/app/components/add-ingredient/add-ingredient.component.ts
+++ src/app/components/add-ingredient/add-ingredient.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IngredientService } from 'src/app/services/ingredient.service';
 
 @Component({
   selector: 'app-add-ingredient',
@@ -7,8 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddIngredientComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private ingredientService: IngredientService) { }
 
-  ngOnInit() {}
+  addForm: FormGroup;
+  submitted = false;
 
+  ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      id: [],
+      nom: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.addForm.valid) {
+      this.ingredientService.addIngredient(this.addForm.value)
+        .subscribe( data => {
+          console.log(data);
+          this.router.navigate(['home']).then(() => {
+            window.location.reload();
+          });
+        });
+    }
+  }
+
+  get f() {
+    return this.addForm.controls;
+  }
 }
